d132390a4b0f22ed3450c8b6bc4161645b1a9101
OlenaPanf <elena.el@ukr.net>
2024-06-21 17:25:55 +0300

    add3

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -18,47 +18,51 @@ const authSlice = createSlice({
   extraReducers: builder => {
     builder
       .addCase(register.pending, state => {
-        state.isLoading = true; // Встановлюємо загальний стан завантаження в true під час очікування завершення реєстрації
-        state.isRefreshing = true; // Встановлюємо стан оновлення в true під час очікування завершення реєстрації
+        state.isLoading = true; // Встановлюємо стан завантаження
+        state.isRefreshing = true; // Встановлюємо стан оновлення
       })
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
-        state.isLoading = false; // Скидаємо загальний стан завантаження після успішної реєстрації
+        state.isLoading = false; // Скидаємо стан завантаження
         state.isLoggedIn = true;
-        state.isRefreshing = false; // Скидаємо стан оновлення після успішної реєстрації
+        state.isRefreshing = false; // Скидаємо стан оновлення
       })
       .addCase(register.rejected, (state, action) => {
-        state.isRefreshing = false; // Скидаємо стан оновлення після невдалої спроби реєстрації
-        console.error(action.error); // Логовуємо помилку, якщо реєстрація не вдалася
-        state.isLoading = false; // Скидаємо загальний стан завантаження після невдалої спроби реєстрації
+        state.isRefreshing = false; // Скидаємо стан оновлення
+        console.error(action.error); // Логовуємо помилку
+        state.isLoading = false; // Скидаємо стан завантаження
       })
       .addCase(logIn.pending, state => {
-        state.isLoading = true; // Встановлюємо загальний стан завантаження в true під час очікування завершення входу в систему
-        state.isRefreshing = true; // Встановлюємо стан оновлення в true під час очікування завершення входу в систему
+        state.isLoading = true; // Встановлюємо стан завантаження
+        state.isRefreshing = true; // Встановлюємо стан оновлення
       })
       .addCase(logIn.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
-        state.isLoading = false; // Скидаємо загальний стан завантаження після успішного входу в систему
-        state.isRefreshing = false; // Скидаємо стан оновлення після успішного входу в систему
+        state.isLoading = false; // Скидаємо стан завантаження
+        state.isRefreshing = false; // Скидаємо стан оновлення
       })
       .addCase(logOut.fulfilled, state => {
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
+        state.isLoading = false; // Скидаємо стан завантаження
       })
       .addCase(refreshUser.pending, state => {
         state.isRefreshing = true;
+        state.isLoading = true; // Встановлюємо стан завантаження
       })
       .addCase(refreshUser.fulfilled, (state, action) => {
         state.user = action.payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
+        state.isLoading = false; // Скидаємо стан завантаження
       })
       .addCase(refreshUser.rejected, state => {
         state.isRefreshing = false;
+        state.isLoading = false; // Скидаємо стан завантаження
       });
   },
 });
